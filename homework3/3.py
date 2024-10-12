673accc52523ebd8db668f5b5fe200ef77586b32
GeorgYashkin <n3v3r.er@gmail.com>
2024-10-12 13:09:43 +0500

    3.py

--- homework3/3.py
+++ homework3/3.py
@@ -1,14 +1,14 @@
 list=[]
 print("Введите числа:")
-while (True):
+while True:
     a=input()
     if a=="end":
         break
-    elif a.isalpha():
-        print("Ошибка при вводе числа")
-    else:
+    try:
         list.append(float(a))
+    except ValueError:
+        print("Ошибка при вводе числа")
 print("Нечетные элементы списка:")
 for i in range(len(list)):
     if list[i]%2==1:
-        print(list[i])
+        print(int(list[i]))
